2bbe96e3f028ce13becf8d6d17c096dda7c352db
Luismbr <68781597+Luismbr@users.noreply.github.com>
2020-08-29 21:17:20 -0400

    U4P4

---
+++ LuisMiguelBR.Unidad4.Practica4/Diplomado_MVC_HTML_Helps_Persona/Models/MantenimientoPersona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Diplomado_MVC_HTML_Helps_Persona.Models
+{
+    public class MantenimientoPersona
+    {
+    }
+}
